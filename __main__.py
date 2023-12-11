92afa1d0ebe31ca0249f312a5b6787ecd71cf31b
Swann <goldenland046@gmail.com>
2023-12-11 10:11:11 +0100

    Setup basic bot and cogs loading

---
+++ __main__.py
@@ -0,0 +1,51 @@
+import env
+import discord
+from os import listdir
+from discord.ext import commands, tasks
+from itertools import cycle
+
+intents = discord.Intents.all()
+intents.members = True
+bot = commands.Bot(command_prefix=env.COMMAND_PREFIX, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}#{bot.user.discriminator} (ID: {bot.user.id})')
+    print('------')
+
+status = cycle([
+    'Pythoning',
+    'Doing stuff...',
+    'Being a bot'
+])
+
+@tasks.loop(minutes=1)
+async def change_presence():
+    new_status = next(status)
+    await bot.change_presence(activity=discord.Game(new_status))
+    print('Status changed to ' + new_status)
+
+@change_presence.before_loop
+async def before_change_presence():
+    '''Only runs once'''
+    await bot.wait_until_ready()
+
+@bot.check
+async def block_dms(ctx):
+    '''Block commands in DMs'''
+    return ctx.guild is not None
+
+async def load_cogs():
+    for filename in listdir('./cogs'):
+        if filename.endswith('.py'):
+            print(f'Cog found: {filename[:-3]}')
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+        
+async def main():
+    change_presence.start()
+    await load_cogs()
+    await bot.start(env.TOKEN)
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
