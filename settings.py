a0060158b7f64bb2ea251e4e9d0491795eaeecf6
Swann <goldenland046@gmail.com>
2023-12-13 15:31:53 +0100

    Force env variable to be strings

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import os
 
 # Core
-TOKEN = os.getenv("TOKEN")
-COMMAND_PREFIX = os.getenv("COMMAND_PREFIX")
+TOKEN: str = os.getenv("TOKEN")
+COMMAND_PREFIX: str = os.getenv("COMMAND_PREFIX")
 
 # Debug
-OWNER_ID = os.getenv("OWNER_ID")
-DEV_GUILD_ID = os.getenv("DEV_GUILD_ID")
+OWNER_ID: str = os.getenv("OWNER_ID")
+DEV_GUILD_ID: str = os.getenv("DEV_GUILD_ID")
 
 # IMGFLIP
-IMGFLIP_USERNAME = os.getenv("IMGFLIP_USERNAME")
-IMGFLIP_PASSWORD = os.getenv("IMGFLIP_PASSWORD")
+IMGFLIP_USERNAME: str = os.getenv("IMGFLIP_USERNAME")
+IMGFLIP_PASSWORD: str = os.getenv("IMGFLIP_PASSWORD")
