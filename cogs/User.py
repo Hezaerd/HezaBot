9e2b96fb05cbd6ca66a0b592c4fbfcc65bff6e6a
Swann <goldenland046@gmail.com>
2023-12-13 01:48:39 +0100

    Change user from groupCog to cog

--- cogs/User.py
+++ cogs/User.py
@@ -3,7 +3,7 @@ from discord import app_commands
 import discord
 import time
 
-class User(commands.GroupCog, group_name="user"):
+class User(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.loading_time = time.time()
