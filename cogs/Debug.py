5b531e437db2635aee55053dad48f44a3442d4e0
Swann <goldenland046@gmail.com>
2023-12-15 23:08:08 +0100

    Update debug cogs
    - New info embed design
    - Code cleanup
    - Tree syncing commands

--- cogs/Debug.py
+++ cogs/Debug.py
@@ -1,8 +1,11 @@
-from discord.ext import commands
-import discord
-from sys import version_info as sysv
-import settings
 import time
+from datetime import datetime
+from sys import version_info as sysv
+
+import discord
+from discord.ext import commands
+
+import settings
 
 
 class Debug(commands.Cog):
@@ -21,13 +24,61 @@ class Debug(commands.Cog):
         print(f'  [Debug]: Discord.py version: {discord.__version__}')
         print(f'  [Debug]: Dev guild: {self.dev_guild}')
 
-    @commands.hybrid_command(name="ping", usage=">ping")
+    @commands.hybrid_group(
+        name="debug",
+        usage=">debug [command]"
+    )
+    async def debug(self, ctx: commands.Context) -> None:
+        """Debug utils for the bot (or client)"""
+        pass
+
+    @debug.command(
+        name="ping",
+        usage=">debug ping"
+    )
     async def ping(self, ctx: commands.Context) -> None:
         """:ping_pong: Send a ping request"""
-        embed = discord.Embed(title=":ping_pong: Pong!", description=f'Latency: `{round(self.bot.latency * 1000)}ms`',
-                              color=0x0099ff)
-        embed.set_footer(text=f'{ctx.message.author.name}\n' + f'Today at {ctx.message.created_at.strftime("%I:%M")}',
+        embed = discord.Embed(title=":ping_pong: Pong!",
+                              description=f'Latency: `{round(self.bot.latency * 1000)}ms`',
+                              color=0x0099ff,
+                              timestamp=datetime.utcnow())
+        embed.set_footer(text=f'{ctx.message.author.name}\n',
                          icon_url=ctx.message.author.avatar)
+        await ctx.reply(embed=embed)
+
+    @debug.command(
+        name="info",
+        usage=">debug info"
+    )
+    async def info(self, ctx: commands.Context) -> None:
+        embed = discord.Embed(title="HezaBot",
+                              description="A multi function bot!",
+                              colour=0x00b0f4,
+                              timestamp=datetime.now())
+
+        embed.add_field(name="Owner :crown:",
+                        value=f'[{settings.OWNER}](https://github.com/Hezaerd)',
+                        inline=False)
+        embed.add_field(name="Discord.py :",
+                        value=f'{discord.__version__}',
+                        inline=True)
+        embed.add_field(name="Python :",
+                        value=f'{sysv.major}.{sysv.minor}.{sysv.micro}',
+                        inline=True)
+        embed.add_field(name="Source code :",
+                        value="[Github](https://github.com/Hezaerd/HezaBot)",
+                        inline=True)
+        embed.add_field(name="Tips:",
+                        value="Use `>help` to know where to start!",
+                        inline=True)
+
+        embed.set_thumbnail(
+            url="https://cdn.discordapp.com/avatars/1181640113239900303/b71bc57ef9b51c2ab9e060d8a39eecff.png?size=1024")
+
+        embed.set_footer(text="Made with ❤️ by Hezaerd",
+                         icon_url="https://cdn.discordapp.com/avatars/225942632050720768"
+                                  "/a_360dbeff591c32cb664950aab0a5a6c8.gif?size=1024")
+
         await ctx.reply(embed=embed)
 
     @commands.command(name="invite", aliases=["link"], usage=">invite")
@@ -37,13 +88,21 @@ class Debug(commands.Cog):
             f'**Invite link:** <https://discord.com/api/oauth2/authorize?client_id={self.bot.user.id}&permissions=8'
             f'&scope=bot>')
 
-    @commands.group(name="sync", usage=">sync [dev/global]", hidden=True)
+    @commands.group(
+        name="sync",
+        usage=">sync [dev/global]",
+        hidden=True
+    )
     @commands.is_owner()
     async def sync(self, ctx: commands.Context) -> None:
         """Sync commands"""
         pass
 
-    @sync.command(name="dev", usage=">sync dev", hidden=True)
+    @sync.command(
+        name="dev",
+        usage=">sync dev",
+        hidden=True
+    )
     @commands.is_owner()
     async def sync_dev(self, ctx: commands.Context) -> None:
         """Sync commands to dev guild"""
@@ -51,7 +110,11 @@ class Debug(commands.Cog):
         print(f'  [Debug]: Synced {len(synced)} commands to {settings.DEV_GUILD_ID}')
         await ctx.send(f'Synced {len(synced)} commands to {settings.DEV_GUILD_ID}')
 
-    @sync.command(name="global", usage=">sync global", hidden=True)
+    @sync.command(
+        name="global",
+        usage=">sync global",
+        hidden=True
+    )
     @commands.is_owner()
     async def sync_global(self, ctx: commands.Context) -> None:
         """Sync commands globally"""
